85ab1656544814952aef5d0468dff576a0c8ebee
Pieter Engelbrecht <pgengel@gmail.com>
2017-05-17 23:01:45 +0200

    Added scaffolding for the different website pages.

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -6,6 +6,11 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { AccountLoginComponent } from './components/account-login/account-login.component';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { CustomerFormComponent } from './components/customer-form/customer-form.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { RentalFormComponent } from './components/rental-form/rental-form.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -14,7 +19,12 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        AccountLoginComponent,
+        CustomerListComponent,
+        CustomerFormComponent,
+        MovieListComponent,
+        RentalFormComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
@@ -23,6 +33,11 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'account/login', component: AccountLoginComponent },
+            { path: 'customers', component: CustomerListComponent },
+            { path: 'customers/new', component: CustomerFormComponent },
+            { path: 'movies', component: MovieListComponent },
+            { path: 'rentals/new', component: RentalFormComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ ClientApp/app/components/rental-form/rental-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rental-form',
+  templateUrl: './rental-form.component.html',
+  styleUrls: ['./rental-form.component.css']
+})
+export class RentalFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

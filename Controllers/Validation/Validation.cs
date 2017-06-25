71e79544fdfcc8ee94d319007ff9d3fdde686c02
Pieter Engelbrecht <pgengel@gmail.com>
2017-06-25 21:28:16 +0200

    Added validation class.

---
+++ Controllers/Validation/Validation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidly.Controllers.Validation
+{
+    public static class Validation
+    {
+	    public static void ValidateNotNull(object value, string message)
+	    {
+			if (value == null)
+			{
+				throw new ValidationException(message);
+			}
+		}
+    }
+}
